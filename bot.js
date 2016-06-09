fa7a35f71e4c086a361d609e3bb279a470d9ecc7
Emily <ebosakowski@gmail.com>
2016-06-09 16:25:22 -0500

    Complete set interval, can tweet tweet and random number every 20 seconds

--- bot.js
+++ bot.js
@@ -3,6 +3,23 @@ var twitInfo = require('./config.js');
 
 var twitter = new Twit(twitInfo);
 
-twitter.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  console.log(data);
-});
+function postTweet() {
+  var r = Math.floor(Math.random()*100);
+
+  var tweet = {
+    status: "I am a test tweet, numero " + r + " #bot"
+  }
+
+  twitter.post('statuses/update', tweet , function(err, data, response) {
+    if (err) {
+      console.log("5OMeTh1nG weNt wR0ng");
+    } else {
+      console.log("Tweet sucessful");
+    };
+  });
+}
+
+// postTweet();
+// second parameter is in miliseconds
+// setInterval(postTweet, 1000*60*60*3);
+
