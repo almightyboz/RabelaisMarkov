899c612d1f6c76f48e1f36c907eebcff06d49c72
Emily <ebosakowski@gmail.com>
2016-06-09 16:47:09 -0500

    Start to configure tweet settings

--- bot.js
+++ bot.js
@@ -19,7 +19,24 @@ function postTweet() {
   });
 }
 
+var useUpperCase = function(wordList) {
+  var tempList = Object.keys(wordList).filter(function(word) {
+    return word[0] >= "A" && word[0] <= "Z"
+  })
+  return tempList[~~(Math.random()*tempList.length)]
+}
+
+var setTweetLength = function(sentence) {
+  var tweetLength = Math.floor(Math.random() * 138) + 100;
+  return sentence.split("").length === tweetLength
+}
+
+var MarkovChain = require('markovchain')
+  , fs = require('fs')
+  , quotes = new MarkovChain(fs.readFileSync('./rabelais.txt', 'utf8'))
+
+console.log(quotes.start(useUpperCase).end(setTweetLength).process())
+
 // postTweet();
 // second parameter is in miliseconds
 // setInterval(postTweet, 1000*60*60*3);
-
